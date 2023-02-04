c25a3aada0be6518a8cc6e6dade6a2d0d6227a8f
Naman Garg <66401604+namanlp@users.noreply.github.com>
2023-02-04 21:59:45 +0530

    Improve the time complexity of factor function

--- src/factor.rs
+++ src/factor.rs
@@ -1,15 +1,28 @@
-pub fn factor(input :i64) -> Vec<i64>{
+pub fn factor(input: i128) -> Vec<i128>{
+    // Initialize factors Vector
+    let mut factors : Vec<i128> = Vec::new();
 
-    let count = 2;
+    let mut i : i128 = 1;
 
-    let mut vector = vec![];
+    // Check till i is less than or equal to square root n
+    while i*i <= number{
+        if number % i == 0 {
+            factors.push(i);
 
-    for count in count..input {
-        if input % count == 0 {
-            vector.push(count);
+            // to prevent duplication, if number is perfect square
+            if i*i != number {
+                factors.push(number / i);
+            }
         }
+        i+=1;
     }
 
-    return vector;
+    // It is generally useful to sort the vector
+    // And it will not affect our time complexity,
+    // Because logarithmic time is much less than square root time complexity
 
+    factors.sort();
+
+    // Return the factors Vector as answer
+    return factors;
 }
